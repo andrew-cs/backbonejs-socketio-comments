47d7c1631e0875c4084880f6f9dd00160a3aab40
Nick Nance <nicknance@Nicks-MacBook-Pro-15i.local>
2013-12-23 23:03:19 -0600

    updated default password

--- models/comments.js
+++ models/comments.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');  
 
-mongoose.connect('mongodb://root:bitnami@localhost/backbone-comments');	
+mongoose.connect('mongodb://admin:Admin1234@localhost/backbone-comments');	
 
 // Comments collection
 var CommentSchema = new mongoose.Schema({
